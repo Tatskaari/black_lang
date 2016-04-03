2e09a75c4870fa1f721fe1a8529d0c2c9fe2bf49
Jonathan <jonfpoole@gmail.com>
2016-04-03 20:38:51 +0100

    Updated lexer to support bool operations and integer operations and fixed some issues with loading files

--- src/files.ml
+++ src/files.ml
@@ -5,18 +5,21 @@ let read_file file_name =
 	let rec read_file in_chan file =
 		let line = 
 			try
-				input_line in_chan 
+				Some (input_line in_chan) 
 			with
 			| End_of_file -> 
 					close_in_noerr in_chan;
-					""
+					None
 			| e ->       
 					close_in_noerr in_chan;           
 					raise e 
 		in
 
-		if line = "" then file else
-		read_file in_chan (file ^ line ^ "\n")
+		match line with
+		| Some line ->
+			read_file in_chan (file ^ line ^ "\n")
+		| None ->
+			file
 	in
 	let in_chan = open_in file_name in
 	read_file in_chan ""
--- src/lex.ml
+++ src/lex.ml
@@ -3,22 +3,33 @@ open Printf
 exception BadInput of string
 
 type keyword = 
-	While
-|  	If 
-|   Minus 
-|   Then  
+  While
+| If 
+| Then 
+| Assign 
 
-let keyword_map = [("while", While); ("if", If); ("-", Minus); ("then", Then)]
+type operator = 
+  Minus
+| Add
+| LThan
+| GThan
+| Mul
+| Div
 
 type token =
 	LBrace
 |	RBrace
 |	LParen
 |	RParen
-|	Ident of string
-| 	Keyword of keyword
 |	Assign
+| 	Keyword of keyword
+|   Operator of operator 
+|	Ident of string
 |	Int of int
+
+let keyword_map = [("while", While); ("if", If); ("then", Then); (":=", Assign)]
+let operator_map = [("-", Minus); ("+", Add); ("<", LThan); (">", GThan); ("*", Div)]
+let token_map = [("{", LBrace); ("}", RBrace); ("(", LParen); (")", RParen); (":=", Assign)]
 
 (* token with a line numer *)
 type token_line = token * int
@@ -52,51 +63,39 @@ let get_token_weighting token =
 	| Some Keyword(_) -> 2
 	| _ -> 0
 
-let match_left_brace str = 
-	match (reg_ex_pred "{" str) with
-	| (Some match_str, len) ->
-		(Some LBrace, len)
-	| (None, len) -> (None, len)
-
-let match_right_brace str = 
-	match (reg_ex_pred "}" str) with
-	| (Some match_str, len) ->
-		(Some RBrace, len)
-	| (None, len) -> (None, len)
-
-let match_left_paren str =
-	match (reg_ex_pred "(" str) with
-	| (Some match_str, len) ->
-		(Some LParen, len)
-	| (None, len) -> (None, len)
-
-let match_right_paren str =
-	match (reg_ex_pred ")" str) with
-	| (Some match_str, len) ->
-		(Some RParen, len)
-	| (None, len) -> (None, len)
-
 let match_ident str =
 	match (reg_ex_pred "[a-zA-Z]+" str) with
 	| (Some match_str, len) ->
 		(Some (Ident match_str), len)
 	| (None, len) -> (None, len)
 
-let match_keyword str =
-	let rec match_keyword keyword_map =
-		match keyword_map with
-		| (key_str, key)::keyword_map -> 
+let match_map map str =
+	let rec match_map map =
+		match map with
+		| (key_str, map_entry)::map -> 
 			begin
 				match (reg_ex_pred key_str str) with
 				| (Some match_str, len) ->
-					(Some (Keyword key), len)
+					(Some map_entry, len)
 				| (None, len) -> 
-					match_keyword keyword_map
+					match_map map
 			end
 		| [] -> 
 			(None, 0)
 	in
-	match_keyword keyword_map
+	match_map map
+
+ let match_token = match_map token_map
+
+ let match_keyword str = 
+ 	match match_map keyword_map str with
+ 	| (Some keyword, len) -> (Some (Keyword keyword), len)
+ 	| (None, len) -> (None, len)
+
+ let match_operator str = 
+ 	match match_map operator_map str with
+ 	| (Some op, len) -> (Some (Operator op), len)
+ 	| (None, len) -> (None, len)
 
 let match_int str =
 	match (reg_ex_pred "[0-9]+" str) with
@@ -104,9 +103,9 @@ let match_int str =
 		(Some (Int (int_of_string match_str)), len)
 	| (None, len) -> (None, len)
 
-let (token_preds:token_pred list) = [match_left_paren; match_right_paren; match_left_brace; match_right_brace; match_keyword; match_ident; match_int]
+let (token_preds:token_pred list) = [match_token; match_keyword; match_ident; match_int; match_operator]
 
-let rec find_longest_match str (longest_match: token option * int) token_preds =
+let rec find_longest_match str (longest_match: token option * int) token_preds line =
 	match token_preds with
 	| pred::token_preds -> 
 		let (match_token, match_len) as new_match = pred (str) in
@@ -122,12 +121,12 @@ let rec find_longest_match str (longest_match: token option * int) token_preds =
 			else
 				longest_match
 		in
-		find_longest_match str longest_match token_preds
+		find_longest_match str longest_match token_preds line
 	| [] -> 
 		begin 
 			match longest_match with
 			| (Some token, len) -> (token, len)
-			| (None, _) -> raise (BadInput ("Unexpected input"))
+			| (None, _) -> raise (BadInput ("Unexpected input on line " ^ (string_of_int line)))
 		end
 
 let rec trim_program program line_number =
@@ -150,7 +149,7 @@ let rec lex (program : string) (token_lines : token_line list) (line : int): tok
 	if program = "" then
 		token_lines
 	else
-		let (token, len) = find_longest_match program (None, 0) token_preds in
+		let (token, len) = find_longest_match program (None, 0) token_preds line in
 		let total_len = String.length program in
 		let program = String.sub program len (total_len-len) in
 		lex program (token_lines@[(token, line)]) line
